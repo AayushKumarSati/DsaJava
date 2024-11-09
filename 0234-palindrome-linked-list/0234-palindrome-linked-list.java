9ff4908002814230b266d8fea3a3ae5e164a44f6
Aayush Kumar Sati <128712771+AayushKumarSati@users.noreply.github.com>
2024-11-09 19:09:27 +0530

    Time: 6 ms (54.44%), Space: 68.7 MB (25.56%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,30 +9,88 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        //By using the stack 
-        //step1:push all the nodes in the stack
-        //traverse from ll while poping from stack
-        Stack<Integer>st=new Stack<>();
-        ListNode node=head;
-        //step1:
-        while(node!=null){
-            st.push(node.val);
-            node=node.next;
+    // find the middle node of the list
+    ListNode middle(ListNode head) {
+        ListNode temp = head;
+        if (head == null) {
+            return head;
         }
-        //step2:
-        ListNode ptr=head;
-        while(st.size()>0){
-            int val1=ptr.val;
-            int val2=st.pop();
-            if(val1!=val2){
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    // reverse the list
+    ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nbr = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nbr;
+
+        }
+        return prev;
+
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        /*
+         * Approach 1: //By using the stack
+         * //step1:push all the nodes in the stack
+         * //traverse from ll while poping from stack
+         * Stack<Integer>st=new Stack<>();
+         * ListNode node=head;
+         * //step1:
+         * while(node!=null){
+         * st.push(node.val);
+         * node=node.next;
+         * }
+         * //step2:
+         * ListNode ptr=head;
+         * while(st.size()>0){
+         * int val1=ptr.val;
+         * int val2=st.pop();
+         * if(val1!=val2){
+         * return false;
+         * 
+         * }
+         * ptr=ptr.next;//move the pointer
+         * }
+         * return true;
+         */
+
+        /*
+         * Approach 2: find the middle from the linked list and than reverse the LL and
+         * check palindrome O(N) time complexity and O(1) space-complexity
+         */
+        if (head == null || head.next == null) {
+            return true;
+        }
+        // find mid
+        ListNode mid = middle(head);
+        // reverse the head
+        ListNode shead = reverse(mid);
+        // check the is palindrome or not
+        ListNode fhead = head;
+        while (shead != null) {
+            if (fhead.val != shead.val) {
                 return false;
 
             }
-            ptr=ptr.next;//move the pointer
+            fhead = fhead.next;
+            shead = shead.next;
         }
         return true;
 
-        
     }
+
 }
