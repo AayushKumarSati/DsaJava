5728d4593fbc27d050d133c2c306493703b6a42c
Aayush Kumar Sati <128712771+AayushKumarSati@users.noreply.github.com>
2024-11-12 14:53:11 +0530

    Time: 5 ms (65.22%), Space: 57.9 MB (69.4%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,31 +1,46 @@
 class Solution {
     public int findDuplicate(int[] arr) {
-        Arrays.sort(arr);
-        int n = arr.length;
-        /*1st approach
-        // for(int i=0; i<n-1; i++){
-        //     if(arr[i]==arr[i+1]){
-        //         return arr[i];
-        //     }
+
+        /*
+         * 1st approach=>O(n^2)
+         * // Arrays.sort(arr);
+         * // int n = arr.length;
+         * // for(int i=0; i<n-1; i++){
+         * // if(arr[i]==arr[i+1]){
+         * // return arr[i];
+         * // }
+         * // }
+         */
+
+        /* 2nd approach using two pinter */
+        // int start=0,end=1;
+        // while(start<n-1 && end<n){
+        // if(arr[start]==arr[end]){
+        // return arr[start];
         // }
-        */
+        // start++;
+        // end++;
+        // }
+        // return -1;
 
-
-        /*2nd approach using two pinter*/
-        int start=0,end=1;
-        while(start<n-1 && end<n){
-            if(arr[start]==arr[end]){
-                return arr[start];
+        /* approach number 3 */
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int ele = arr[i];
+            ele = Math.abs(ele);// negative to positive
+            if (arr[ele] > 0) {
+                // if elements is positive than negative the value
+                arr[ele] = -arr[ele];
+            } else {
+                ans = ele;
+                break;
             }
-            start++;
-            end++;
         }
-        return -1;
-       
- 
-       
-    
-
+        /* modify the negative arrays */
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.abs(arr[i]);
+        }
+        return ans;
 
     }
 }
