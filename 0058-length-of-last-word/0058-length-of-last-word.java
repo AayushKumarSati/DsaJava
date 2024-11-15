c672eb47850f8bcc6627fe20a9fab2378ccdb56c
Aayush Kumar Sati <128712771+AayushKumarSati@users.noreply.github.com>
2024-11-15 18:22:47 +0530

    Time: 0 ms (100%), Space: 41.4 MB (90.86%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,16 +1,33 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        StringBuilder ans = new StringBuilder();
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (arr[i].length() == 0) {
-                continue;
-            }
-            if (ans.length() == 0) {
-                ans.append(arr[i]);
+        /* approach 1: by using the stringBuilder */
+        /*
+         * String[] arr = s.split(" ");
+         * StringBuilder ans = new StringBuilder();
+         * for (int i = arr.length - 1; i >= 0; i--) {
+         * if (arr[i].length() == 0) {
+         * continue;
+         * }
+         * if (ans.length() == 0) {
+         * ans.append(arr[i]);
+         * 
+         * }
+         * }
+         * return ans.length();
+         */
 
+        /** Appraoch 2: */
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            if (ch != ' ') {
+                count++;
+
+            } else if (count != 0) {
+                break;
             }
+
         }
-        return ans.length();
+        return count;
     }
 }
