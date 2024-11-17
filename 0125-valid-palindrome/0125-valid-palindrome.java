7c25b1e2b81d803f428cf8286ba3e156443c5b0b
Aayush Kumar Sati <128712771+AayushKumarSati@users.noreply.github.com>
2024-11-17 21:34:05 +0530

    Time: 3 ms (72.38%), Space: 43.2 MB (58.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,38 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        String str=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-        StringBuilder sb1=new StringBuilder(str);
-        sb1.reverse();
-        String result=sb1.toString();
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)!=result.charAt(i)){
+    public boolean isPalindrome(String str) {
+        // handle the edges case if s==0 return true
+        if (str.length() == 1) {
+            return true;
+        }
+        // convert the upper case to lower case
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            // character already in lower case
+            if (ch >= 'a' && ch <= 'z') {
+                sb.append(ch);
+            } else if (ch >= 'A' && ch <= 'Z') {
+                // convert upper case to lower case
+                ch = (char) (ch - 'A' + 'a');
+                sb.append(ch);
+
+            } else if (ch >= '0' && ch <= '9') {
+
+                sb.append(ch);
+
+            }
+        }
+
+        // check the new lowercase string palindrome or not
+        int start = 0, end = sb.length() - 1;
+        while (start < end) {
+            char startingchar = sb.charAt(start);
+            char endchar = sb.charAt(end);
+            if (startingchar != endchar) {
                 return false;
             }
+            start++;
+            end--;
         }
         return true;
     }
