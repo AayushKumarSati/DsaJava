d730e55818e8982b4cfdc1cd113932fcc7f66d99
Aayush Kumar Sati <128712771+AayushKumarSati@users.noreply.github.com>
2025-01-05 22:19:20 +0530

    Time: 4 ms (9.54%), Space: 42.3 MB (54.48%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -4,15 +4,36 @@ class Solution {
          * Using Broot Force Approach
          * Time Complexity : O(N^2) & Space Complexity :O(1)
          */
-        int count = 0;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                int sum = nums.get(i) + nums.get(j);
-                if (sum < target) {
-                    count++;
-                }
+        // int count = 0;
+        // for (int i = 0; i < nums.size() - 1; i++) {
+        // for (int j = i + 1; j < nums.size(); j++) {
+        // int sum = nums.get(i) + nums.get(j);
+        // if (sum < target) {
+        // count++;
+        // }
+        // }
+        // }
+        // return count;
+
+        /*
+         * Using Two pointer(Optimal) Approach
+         * Time Complexity : O(nlogn) & Space Complexity :O(1)
+         */
+        int count = 0, left = 0, right = nums.size() - 1;
+        // sort the list
+        nums.sort((a, b) -> a - b);
+        while (left < right) {
+            int sum = nums.get(left) + nums.get(right);
+            if (sum < target) {
+                // if left and right ptr sum is less than target than considerd all the elements
+                // count which is present left to right pointer mid
+                count += right - left;
+                left++;
+            } else {
+                right--;
             }
         }
         return count;
+
     }
 }
