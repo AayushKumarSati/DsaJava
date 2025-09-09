c5824739a36b6211c686f6795e914f0f0331f0dc
Aayush Kumar Sati <128712771+AayushKumarSati@users.noreply.github.com>
2025-09-09 12:48:29 +0530

    Time: 4 ms (49.06%), Space: 52.7 MB (9.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -16,37 +16,44 @@ class Solution {
             for (int j = 0; j < c; j++) {
                 if (grid[i][j] == '1') {
                     island++;
-                    // make all one convert to zero
-                    q.add(new int[] { i, j });
-                    grid[i][j] = '0';
-
-                    // bfs call
-                    while (q.size() > 0) {
-                        int pair[] = q.poll();
-                        int new_i = pair[0];
-                        int new_j = pair[1];
-                        // and make all adjacent 1 by 0
-                        //4 in the leetcode only four direction
-                        // direction{up,down,left,right)
-                        // directions: up, down, left, right
-                        int row[] = { -1, 1, 0, 0 };
-                        int col[] = { 0, 0, -1, 1 };
-                        //check all 8 neighbours 
-                        for (int k = 0; k < 4; k++) {
-                            int newrow = new_i + row[k];
-                            int newcol = new_j + col[k];
-
-                            //  If neighbor is valid and is land ('L'), visit it
-                            if (valid(newrow, newcol) && grid[newrow][newcol] == '1') {
-                                grid[newrow][newcol] = '0';
-                                q.add(new int[] { newrow, newcol });
-                            }
-                        }
-
-                    }
+                    bfsCall(grid, i, j);
                 }
+
             }
         }
         return island;
+    }
+
+    private void bfsCall(char[][] grid, int startrow, int startcol) {
+        Queue<int[]> q = new LinkedList<>();
+        // make all one convert to zero
+        q.add(new int[] { startrow, startcol });
+        grid[startrow][startcol] = '0';
+
+        // bfs call
+        while (q.size() > 0) {
+            int pair[] = q.poll();
+            int new_i = pair[0];
+            int new_j = pair[1];
+            // and make all adjacent 1 by 0
+            //4 in the leetcode only four direction
+            // direction{up,down,left,right)
+            // directions: up, down, left, right
+            int row[] = { -1, 1, 0, 0 };
+            int col[] = { 0, 0, -1, 1 };
+            //check all 8 neighbours 
+            for (int k = 0; k < 4; k++) {
+                int newrow = new_i + row[k];
+                int newcol = new_j + col[k];
+
+                //  If neighbor is valid and is land ('L'), visit it
+                if (valid(newrow, newcol) && grid[newrow][newcol] == '1') {
+                    grid[newrow][newcol] = '0';
+                    q.add(new int[] { newrow, newcol });
+                }
+            }
+
+        }
+
     }
 }
